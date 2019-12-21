be972e83eca0e22721d18d109c4b1df81686b455
KirillKandrashin <59111411+KirillKandrashin@users.noreply.github.com>
2019-12-21 18:15:31 +0300

    Update parsing.py

--- parsing.py
+++ parsing.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import re
 import sqlite3
-conn=sqlite3.connect('mydatabase.db')                    #создаем базу данных с именем mydatabase
+conn=sqlite3.connect('cinemas.db')                    #создаем базу данных с именем mydatabase
 cursor=conn.cursor()                                    #cursor позволяет нам работать с базой данных
 
 def delete_tables(cursor):                            #удаляем таблицы, созданные ранее
